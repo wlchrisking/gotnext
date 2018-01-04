5d6d016015fc6e78f25653fbff117001716e155c
Avi Cohen <acemail10@gmail.com>
2018-01-04 15:47:37 -0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const db = new Sequelize(process.env.PG);
 db.authenticate()
   .then( () => {
       console.log('server.js: DB is now connected');
+      console.log('DB is doubly-confirmed to be connected')
   })
   .catch( (err) => {
       console.log('server.js: Connection Error!');
