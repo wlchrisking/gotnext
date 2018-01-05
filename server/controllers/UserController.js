b49c905474df4543a4967561a7a3ad660e6ce665
Patrick Wu <pkwu@berkeley.edu>
2018-01-04 16:22:58 -0800

    [Add] - Try to add merge conflict

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -7,6 +7,9 @@ UserController = {
     res.send();
     res.end(); //grinded.
 
+    console.log('hello sun');
+    console.log('hello world');
+    console.log('hello moon');
   },
 
   Logout: (req, res) => {
