32fcefd0a42bdd16e8db1bc57fed00a0660fa7c8
Chris King <chrisking858@gmail.com>
2018-01-09 14:38:04 -0800

    [cleanup] cleaned up comments

--- server/routes.js
+++ server/routes.js
@@ -63,46 +63,7 @@ Router.route('/games/delete')
 
 // below is middleware to check if token exists on client request. 
 // all routes below this function must have a token.
-
-
 Router.use(tokenExists);
-
-// Router.use(function (req, res, next) {
-//   // check header or url parameters or post parameters for token
-//   // we are set up to have the token stored in req.body.token
-//   var token = req.token || req.body.token || req.query.token || req.headers.token || req.headers['x-access-token'];
-//   console.log('Current Token:', req.body.token) 
-//   // decode token
-//   if (token) {
-
-//     // verifies secret and checks exp
-//     jwt.verify(token, 'brianlovestodeleteapps', function (err, decoded) {
-//       if (err) {
-//         console.log('Token Error:', err.name + ' ' + err.message)
-//         return res.json({ success: false, message: 'Failed to authenticate token.' });
-//       } else {
-//         console.log('Token is still active!')
-//         // if everything is good, save to request for use in other routes
-//         console.log('req.decoded before:', req.decoded)
-//         req.decoded = decoded;
-//         console.log('req.decoded after:', req.decoded)
-//         console.log('decoded:', decoded)
-        
-//         next();
-//       }
-//     });
-
-//   } else {
-
-//     // if there is no token
-//     // return an error
-//     return res.status(200).send({
-//       success: false,
-//       message: 'No token provided.'
-//     });
-
-//   }
-// });
 
 Router.route('/games/create')
 .post(GamesController.CreateGame);
