33fe26e2b8ec66cb6ef3002f7de2940e788b49d2
Brian <ebrianhong@gmail.com>
2018-01-08 21:30:32 -0800

    [Feature] - store lat/lng coordinates as 'location' state upon clicking the map

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux'; 
-import {fetchGameData} from '../actions/fetchGameData.js'
 
 class Login extends Component {
   render() {
--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,44 +1,55 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux'; 
+
+import {setLocation} from '../actions/setLocation';
+
+// google-maps-react library stuff
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import googleApi from '../config/config.js'
 
-import {
-  withGoogleMap, 
-  GoogleMap, 
-  InfoWindow, 
-  Marker,
-  withScriptjs
-} from 'react-google-maps';
-import MyMapComponent from'./MyMapComponent';
 
-import {bindActionCreators} from 'redux'; 
-import {fetchGameData} from '../actions/fetchGameData.js'
+class Maps extends Component {
 
-class Map extends Component {
   render() {
-    return(
-      <div>
-        Map
-        <MyMapComponent
-          isMarkerShown
-          googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${googleApi.key}`}
-          loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `400px` }} />}
-          mapElement={<div style={{ height: `100%`, width: '100%' }} />}
-        />
-      </div>
-    )
+    return (
+      <Map 
+        onClick={(mapProps, map, clickEvent) => {
+          // upon clicking the map, store the lat/lng coordinates as 'location'
+          console.log({lat: clickEvent.latLng.lat(), lng: clickEvent.latLng.lng()})
+          this.props.setLocation({'lat': clickEvent.latLng.lat(), 'lng': clickEvent.latLng.lng()})
+        }}
+        google={this.props.google} 
+        zoom={14}
+        style={{
+          width: '40%',
+          height: '80%'
+        }} 
+        // hack reactor as initial location
+        initialCenter={{
+          lat: 33.976215,
+          lng: -118.390891          
+        }}
+      >
+      </Map>
+    );
   }
 }
 
 const mapStateToProps = state => {
   return {
-    
+    location: state.location
   }
 };
 
-export default connect(mapStateToProps)(Map);
-// export default Map;
+const matchDispatchToProps = dispatch => {
+  return bindActionCreators({
+    setLocation:setLocation}, 
+    dispatch);
+};
 
+const WrappedContainer = GoogleApiWrapper({
+  apiKey: (googleApi.key)
+})(Maps);
 
-
+export default connect(mapStateToProps, matchDispatchToProps)(WrappedContainer)
