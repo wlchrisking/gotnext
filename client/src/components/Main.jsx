a998b7334d1006da9bdeac805b2e73546a145543
Chris King <chrisking858@gmail.com>
2018-01-12 13:01:22 -0800

    [merge] fixed my own merge conflicts

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-<<<<<<< HEAD
-import { Media, orm, FormControl, Grid, Button, Jumbotron, Row, Col, FormGroup, ControlLabel, HelpBlock } from 'react-bootstrap';
-=======
->>>>>>> testmerging
 import Maps from './Map.jsx';
 import Option from './Option.jsx';
 import Login from './Login.jsx';
@@ -32,26 +28,6 @@ class Main extends Component {
 
     if (this.props.loginPage === 'default' || this.props.loginPage === null) {
       return (
-<<<<<<< HEAD
-        <div style={{border: '2px solid pink'}}>
-          <Col xs={6} md={6}>
-            All Games:
-            <Option />
-          </Col>
-          <br /><br />
-          <br /><br />
-
-          <Col>
-            Map of Games:
-            <Maps />
-          </Col>
-
-          <br /><br />
-          <br /><br />
-          <div>
-            {JSON.stringify(this.props.location)}
-          </div>
-=======
         <div>
           <Col md={6} style={{ border: '2px solid pink' }}>
             <Option />
@@ -61,7 +37,6 @@ class Main extends Component {
             Map:
           <Maps />
           </Col>
->>>>>>> testmerging
         </div>
       )
     }
