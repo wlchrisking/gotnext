23ab8cceff1162d182c58e50a0726551ebd93c70
Avi Cohen <acemail10@gmail.com>
2018-01-13 10:20:33 -0800

    [fix] - fix env var

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
-// const db = new Sequelize(process.env.PG);
-const db = new Sequelize(PG);
+const db = new Sequelize(process.env.PG);
+// const db = new Sequelize(PG);
 
 db.authenticate()
   .then( () => {
